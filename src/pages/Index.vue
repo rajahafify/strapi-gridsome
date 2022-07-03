4aa2541beccd27a213872f8c655c34ddd1e9be44
hafify <hafify@airasia.com>
2022-07-04 04:42:15 +0800

    Initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,36 @@
+<template>
+  <Layout>
+    <div v-for="member in members" :key="member.id">
+      <h2>Name: {{ member.attributes.name }}</h2>
+      <p>Email: {{ member.attributes.email }}</p>
+      <p>Phone Number: {{ member.attributes.phoneNumber }}</p>
+    </div>
+  </Layout>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  metaInfo: {
+    title: 'Hello, world!'
+  },
+  data() {
+    return {
+      members: null
+    }
+  },
+  async mounted () {
+    console.log('hello')
+    const { data } = await axios.get('https://strapi-ugwh.onrender.com/api/members')
+    this.members = data.data
+    console.log(this.members)
+  }
+}
+
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
